6a49b1c96a4be8983eb014784a31186100434e89
dninemfive <11084010+dninemfive@users.noreply.github.com>
2022-12-01 11:50:43 -0800

    figure out how i fucked up my math; positioning confirmed to work for n = 1

--- Assets/Combat/CharacterDisplayer.cs
+++ Assets/Combat/CharacterDisplayer.cs
@@ -17,14 +17,15 @@ namespace JamazonBrine
             Debug.Log($"{Side}CharacterDisplayer.LoadScenario({scenario.Name})");
             List<Character> characters = scenario.CharactersOn(Side).ToList();
             Rect rect = GetComponent<RectTransform>().rect;
-            float offset = rect.height / characters.Count, totalOffset = offset;
+            float offset = rect.height / (characters.Count + 1), totalOffset = offset;
+            Debug.Log($"Offset is {offset}");
             foreach(Character c in characters)
             {
                 Debug.Log($"Adding character {c.Name}...");
                 GameObject co = c.InstantiateCharacter(CharacterPrefab);
                 co.transform.SetParent(transform);
                 Debug.Log($"Set {c.Name}'s parent to {gameObject.name}");
-                Vector3 tf = new(transform.position.x, rect.yMax - totalOffset);
+                Vector3 tf = new(transform.position.x, rect.height - totalOffset);
                 totalOffset += offset;
                 Debug.Log($"{c.Name}'s internal position is {tf}");
                 co.transform.position = tf;
